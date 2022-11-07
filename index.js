22d6b640464b94c24a992c6ff9f973146dc73887
Ahlam Alharbi <87574448+cs1ahlam@users.noreply.github.com>
2022-11-08 02:16:40 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports.add = add;
+module.exports.multiply = multiply;
+module.exports.subtract = subtract;
+module.exports.divide = divide;
+module.exports.power = power;
+module.exports.factorial = factorial;
+module.exports.modolo = modolo;
+module.exports.check_odd_number = check_odd_number;
+module.exports.even = even;
+
+function add(...number) {
+    let sum = 0;
+    number.forEach(x => sum += x);
+    return sum;
+}
+
+function multiply(...number) {
+    sum = 1
+    number.forEach(x => sum *= x);
+    return sum
+}
+
+function subtract(number1, number2) {
+    return number1 - number2
+}
+
+function divide(number1, number2) {
+    return number1 / number2
+}
+
+function power(base, power) {
+    return base ** power
+}
+
+
+function factorial(number) {
+    sum = 1
+    for (let i = number; i > 0 ; i--) {
+        sum *= i
+    }
+    return sum
+}
+
+function modolo(number1, number2) {
+	return number1 % number2;
+}
+
+function check_odd_number(number) {
+    if (number % 2 != 0) {
+        return true
+    }
+    return false
+}
+
+function even(number) {
+	if(number % 2 == 0) {
+    return "even"
+}
+}
