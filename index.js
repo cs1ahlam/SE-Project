cb7922695a00103248d053747cbe3e913e2b7c2f
Rahaf Alnassar <117217097+Rahaf-aln@users.noreply.github.com>
2022-11-08 03:33:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports.modolo = modolo;
 module.exports.check_odd_number = check_odd_number;
 module.exports.even = even;
 
-function add(...number) {
+function add(...number {
     let sum = 0;
     number.forEach(x => sum += x);
     return sum;
